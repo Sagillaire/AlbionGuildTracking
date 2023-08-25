7ee8de5b29a2f7af27686caa24117d1c75e0dfb3
Juan Cardona <juanscr24@hotmail.com>
2023-08-25 00:50:45 -0500

    Adding CardPost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { CardPost } from '@/components/CardPost';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}></main>
+    <main className={styles.main}>
+      <CardPost title='Albion'/>
+    </main>
   )
 }
