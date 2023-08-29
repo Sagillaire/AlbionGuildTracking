532d9fa5fd238e9a6527a84bfc43420b35350fc6
Sagillaire <Jasantise@outlook.es>
2023-08-28 23:33:27 -0500

    Move correspondents folders modasl create route hooks

--- src/views/AdminPanel/AdminMapRoutes/Hooks/useModal.ts
+++ src/views/AdminPanel/AdminMapRoutes/Hooks/useModal.ts
@@ -1,4 +1,36 @@
-export const useModal = () => {
+import { message } from 'antd';
+import { AxiosError } from "axios";
+import { useMutation } from "react-query";
+import { MapRoute, userStore } from "@/core";
+import { useFieldArray, useForm } from "react-hook-form";
 
-    return {}
+export const useModal = (onCancel: () => void) => {
+    const { control, handleSubmit, formState: { errors }, watch, reset } = useForm()
+    const { user } = userStore()
+
+    const { mutateAsync } = useMutation((data) => MapRoute.post('', data), {
+        onSuccess: () => {
+            message.success('Ruta creada')
+            reset()
+        },
+        onError(error: AxiosError<{ message: string }>) {
+            message.error(`Error de petición. ${error?.message}`)
+        }
+    })
+
+    const createMapRoute = (form: any) => {
+        const data = { ...form, created_by: user }
+        return mutateAsync(data)
+    }
+
+    const { fields: routes, append, remove } = useFieldArray({
+        control: control,
+        name: 'route_info'
+    })
+
+    const handleCancel = () => {
+        onCancel()
+        reset()
+    }
+    return { handleCancel, handleSubmit, createMapRoute, append, routes, control, watch, remove, errors }
 }
--- src/views/AdminPanel/AdminMapRoutes/ModalCreate.tsx
+++ src/views/AdminPanel/AdminMapRoutes/ModalCreate.tsx
@@ -1,46 +1,16 @@
 import { FC } from 'react';
-import { AxiosError } from "axios";
+import { Modal } from 'antd';
+import { useModal } from './Hooks';
 import { IModalCreate } from './types';
 import { GoTrash } from 'react-icons/go';
-import { FaRoute } from 'react-icons/fa';
 import styles from './styles.module.css';
 import { Mapvalues } from '@/core/utils';
-import { useMutation } from "react-query";
-import { MapRoute, userStore } from "@/core";
-import { Button, Modal, message } from 'antd';
-import { CustomInput, CustomSelect } from "@/components";
-import { useFieldArray, useForm } from "react-hook-form";
-import { colorOptions, colourStyles } from '@/components/Select/constants';
 import { TitleNode } from './Components/TitleNode';
+import { CustomInput, CustomSelect } from "@/components";
+import { colorOptions, colourStyles } from '@/components/Select/constants';
 
 export const ModalCreate: FC<IModalCreate> = ({ open, onCancel }) => {
-    const { control, handleSubmit, formState: { errors }, watch, reset } = useForm()
-    const { user } = userStore()
-
-    const { mutateAsync } = useMutation((data) => MapRoute.post('', data), {
-        onSuccess: () => {
-            message.success('Ruta creada')
-            reset()
-        },
-        onError(error: AxiosError<{ message: string }>) {
-            message.error(`Error de petición. ${error?.message}`)
-        }
-    })
-
-    const createMapRoute = (form: any) => {
-        const data = { ...form, created_by: user }
-        return mutateAsync(data)
-    }
-
-    const { fields: routes, append, remove } = useFieldArray({
-        control: control,
-        name: 'route_info'
-    })
-
-    const handleCancel = () => {
-        onCancel()
-        reset()
-    }
+    const { handleCancel, handleSubmit, createMapRoute, append, routes, control, watch, remove, errors } = useModal(onCancel)
 
     return (
         <Modal
