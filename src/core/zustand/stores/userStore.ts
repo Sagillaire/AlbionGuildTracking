c6effd19faefe65f702db85e3506b7d8ed66ecf3
Sagillaire <Jasantise@outlook.es>
2023-10-05 23:48:56 -0500

    Feature: userStore docs

--- src/core/zustand/stores/userStore.ts
+++ src/core/zustand/stores/userStore.ts
@@ -1,20 +1,37 @@
 import { create } from "zustand";
 import { IUserStore, IGetSessionAction } from "./userStore.types";
 
+/**
+ * Represents the user store for managing user-related state.
+ */
 export const userStore = create<IUserStore>((set) => ({
     rol: '',
     user: '',
     isAuth: false,
     loading: false,
     verified: false,
+
+    /**
+     * Set the loading state of the user.
+     * @param isLoad - A boolean value indicating whether data is being loaded.
+     */
     setUserLoad: (isLoad: boolean) => set((payload) => ({
         ...payload,
         loading: isLoad
     })),
+
+    /**
+     * Log the user out and clear their session data.
+     */
     logout: () => {
         set(() => ({ rol: '', user: '', isAuth: false, loading: false, verified: false }))
         localStorage.removeItem('guildUserToken')
     },
+
+    /**
+     * Get the user's session data and update the store state.
+     * @param data - An object containing session data.
+     */
     getSession: (data: IGetSessionAction) => set((payload) => ({
         ...payload,
         rol: data?.rol,
