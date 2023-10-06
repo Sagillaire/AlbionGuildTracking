9ea3e19c457369dbadbfc83828695b3fd413ab0d
Sagillaire <Jasantise@outlook.es>
2023-10-05 23:51:37 -0500

    Feature: useSafeHydrate docs

--- src/core/Hooks/usesafeHydrate.tsx
+++ src/core/Hooks/usesafeHydrate.tsx
@@ -1,6 +1,12 @@
 'use client'
 import { FC, useEffect, useState } from 'react';
 
+/**
+ * SafeHydrate is a React component that safely hydrates children during server-side rendering (SSR).
+ * It ensures that the children are hydrated only on the client-side, preventing potential mismatches
+ * between server-rendered and client-rendered content.
+ * @param children - The JSX elements to be safely hydrated.
+ */
 export const SafeHydrate: FC<{ children: JSX.Element | JSX.Element[] }> = ({
     children
 }) => {
