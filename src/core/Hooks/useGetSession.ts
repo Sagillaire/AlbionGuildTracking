d84adbdcb040360857d85dd10213ae28fcbc74da
Sagillaire <Jasantise@outlook.es>
2023-10-05 23:55:02 -0500

    Feature: useGetSession docs

--- src/core/Hooks/useGetSession.ts
+++ src/core/Hooks/useGetSession.ts
@@ -4,6 +4,10 @@ import { useMutation } from "react-query";
 import { useCallback, useEffect } from "react";
 import { GetSessionService } from "../Services";
 
+/**
+ * Custom hook for getting user session data and handling loading state.
+ * @returns An object with a function to trigger session retrieval.
+ */
 export const useGetSession = () => {
     const { getSession, setUserLoad } = userStore()
     const { mutateAsync, isLoading } = useMutation((token) => GetSessionService.post('', { token }), {
