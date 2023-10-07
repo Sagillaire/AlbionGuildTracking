848651008baead1edbbc4acfd212172e060fad86
Sagillaire <Jasantise@outlook.es>
2023-10-06 23:06:42 -0500

    Feature: apiServicevalidation docs

--- src/core/ApiService/ApiServiceValidations.ts
+++ src/core/ApiService/ApiServiceValidations.ts
@@ -12,10 +12,22 @@ interface IResponse {
     status: number;
 }
 
+/**
+ * Handles a successful response from an Axios request.
+ *
+ * @param {AxiosResponse} response - The successful Axios response.
+ * @returns {Promise<AxiosResponse>} - A promise that resolves with the original response.
+ */
 export const handleResponse = (response: AxiosResponse) => {
-    return Promise.resolve(response)
-}
+    return Promise.resolve(response);
+};
 
+/**
+ * Handles an error in an Axios request.
+ *
+ * @param {ErrorResponse | AxiosError} error - The Axios request error.
+ * @returns {Promise<{ message: ErrorResponse | AxiosError }>} - A promise that rejects with an object containing the error message.
+ */
 export const handleError = (error: ErrorResponse | AxiosError) => {
-    return Promise.reject({ message: error })
+    return Promise.reject({ message: error });
 };
