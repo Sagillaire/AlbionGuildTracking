464761234370eb3fc62688c3307625e44e4a6f7e
Sagillaire <Jasantise@outlook.es>
2023-10-06 23:08:08 -0500

    Feature: apiServiceConfig docs

--- src/core/ApiService/ApiServiceConfig.ts
+++ src/core/ApiService/ApiServiceConfig.ts
@@ -2,6 +2,12 @@ import axios, { AxiosInstance } from 'axios';
 import { BaseURL } from "./ApiServiceConfigUrl";
 import { handleError, handleResponse } from "./ApiServiceValidations";
 
+/**
+ * Creates an Axios instance with the specified base URL and default timeout.
+ *
+ * @param {string} baseURL - The base URL for the Axios instance.
+ * @returns {AxiosInstance} - An Axios instance configured with the provided base URL and timeout.
+ */
 const createAxiosInstance = (baseURL: string): AxiosInstance => {
     const instance = axios.create({ baseURL, timeout: 20000 });
     instance.interceptors.response.use(handleResponse, handleError);
@@ -14,9 +20,14 @@ const createAxiosInstance = (baseURL: string): AxiosInstance => {
     return instance;
 };
 
-// AXIOS INSTANCES
+/**
+ * Represents an interface for client instances.
+ */
 const GuildTrackingUrl = createAxiosInstance(BaseURL() as string);
 
+/**
+ * An object containing different Axios client instances.
+ */
 export interface ClientsInterface {
     GuildTrackingUrl: AxiosInstance;
 }
